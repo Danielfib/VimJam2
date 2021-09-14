83c72825076bcb8d6c94ee91993a4eac3eaf0cc6
Daniel <danielfilguera98@gmail.com>
2021-09-14 11:51:18 -0300

    Animating player movement

--- TheOffice/Assets/Scripts/PlayerController.cs
+++ TheOffice/Assets/Scripts/PlayerController.cs
@@ -6,10 +6,20 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rb;
     [SerializeField] float speed;
+    [SerializeField] Animator animator;
+
+    float stressMultiplier = 1f;
 
     void Update()
     {
         ProccessMovement();
+        Animate();
+    }
+
+    private void Animate()
+    {
+        animator.SetBool("IsWalking", rb.velocity != Vector2.zero);
+        animator.speed = Mathf.Clamp(rb.velocity.magnitude / 4, 1, 2) * stressMultiplier;
     }
 
     private void ProccessMovement()
