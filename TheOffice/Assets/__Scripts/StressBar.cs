5ca1c9b89f1a8d44a6c6e4fed408fbce6ba84a06
Daniel <danielfilguera98@gmail.com>
2021-09-14 23:25:27 -0300

    Reworking stressbar and animating it

--- TheOffice/Assets/__Scripts/StressBar.cs
+++ TheOffice/Assets/__Scripts/StressBar.cs
@@ -6,10 +6,27 @@ using UnityEngine.UI;
 
 public class StressBar : Singleton<StressBar>
 {
-    [SerializeField] Slider stressSlider;
+    [SerializeField] private RawImage[] units;
+    [SerializeField] private Animator animator;
+    
+    private float stress;
 
+    //s ranges from 0 to 1
     public void SetStress(float s)
     {
-        stressSlider.value = s;
+        stress = s;
+        animator.SetFloat("Stress", stress);
+        for (int i = 0; i < units.Length; i++)
+        {
+            var tOffset = (float)i / (float)units.Length;
+            if(tOffset < s)
+            {
+                var unitColor = Color.Lerp(Color.green, Color.red, tOffset);
+                units[i].color = unitColor;
+            } else
+            {
+                units[i].color = Color.gray;
+            }
+        }
     }
 }
